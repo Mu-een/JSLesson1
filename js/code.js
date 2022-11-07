16d1fb69b62833e5d9fd07713989bdf74a97b514
Mu-een <mueenslam@gmail.com>
2022-11-07 15:41:12 +0200

    js

---
+++ js/code.js
@@ -0,0 +1,9 @@
+// function madeBy() {
+//     alert("Made By Mu-een Slamat");
+// }
+
+let madeBy = document.querySelector("button");
+// Add listener
+madeBy.addEventListener('click', ()=>{
+    alert("Made By Mu-een Slamat");
+})
